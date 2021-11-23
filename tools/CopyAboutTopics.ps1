86f3c5b32d3eb62b32c14918e0d8ee4bab860439
Beisi Zhou <bez@microsoft.com>
2021-11-23 13:04:41 +0800

    add about_az.help.txt into en-US culture folder

---
+++ tools/CopyAboutTopics.ps1
@@ -0,0 +1,26 @@
+[CmdletBinding()]
+Param(
+    [Parameter()]
+    [string]$BuildConfig = "Release"
+)
+
+function CopyAboutTopicsToCultureFolder {
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$AboutFolder,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [string]$CultureFolder
+    )
+    Write-Host $AboutFolder, $CultureFolder
+    Get-ChildItem -Filter *.help.txt -Path $AboutFolder | Copy-Item -Destination $CultureFolder
+}
+
+$AboutFolders = Get-ChildItem -Include 'About' -Path "$PSScriptRoot\..\artifacts\$BuildConfig" -Recurse -Directory | where { -not [Tools.Common.Utilities.ModuleFilter]::IsAzureStackModule($_.FullName) }
+
+foreach ($AboutFolder in $AboutFolders)
+{
+    $ModuleFolder = (Get-Item $AboutFolders.Parent).Parent.FullName
+    $CultureFolder = Join-Path -Path $ModuleFolder -ChildPath "en-US"
+    New-Item -ItemType "directory" -Path $CultureFolder -Force
+    CopyAboutTopicsToCultureFolder $AboutFolder $CultureFolder
+}
